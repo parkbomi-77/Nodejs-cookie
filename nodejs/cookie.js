f0498fd59ae4f2024c8b1d8907a87bb4446f5dde
parkbomi-77 <vnstn785@naver.com>
2021-06-03 16:39:04 +0900

    permanent쿠키와 쿠키옵션들

--- nodejs/cookie.js
+++ nodejs/cookie.js
@@ -1,14 +1,22 @@
 var http = require('http');
 var cookie = require('cookie');
+
 http.createServer(function (request, response) {
 	console.log(request.headers.cookie);
-	var cookie = {};
+	var cookies = {};
 	if (request.headers.cookie !== undefined) {
 		cookies = cookie.parse(request.headers.cookie);
 	}
 	console.log(cookies.yummy_cookie);
 	response.writeHead(200, {
-		'Set-Cookie': ['yummy_cookie = choco', 'tasty_cookie = strawberry']
+		'Set-Cookie': [
+			'yummy_cookie = choco',
+			'tasty_cookie = strawberry',
+			`permanent = cookies; Max-Age =${60 * 60 * 24 * 30}`,
+			'Secure = secure; Secure',
+			'HttpOmly = httponly; HttpOnly',
+			'Path = Path; Path = /cookie'
+		]
 	});
 	response.end('Cookie!!');
 }).listen(3000);
