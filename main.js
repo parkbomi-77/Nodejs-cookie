d25d20464c67a29b4186823ae9e63a053bdaee80
parkbomi-77 <vnstn785@naver.com>
2021-06-05 22:53:23 +0900

    로그인 상태에 따른 UI변경

--- main.js
+++ main.js
@@ -5,6 +5,28 @@ var qs = require('querystring');
 var template = require('./lib/template.js');
 var path = require('path');
 var sanitizeHtml = require('sanitize-html');
+var cookie = require('cookie');
+
+function authIsOwner(request, response) {
+  var isOwner = false;
+  var cookies = {};
+  if (request.headers.cookie) {
+    cookies = cookie.parse(request.headers.cookie);
+
+  }
+  if (cookies.email === 'pak@gamil.com' && post.password === '111111') {
+    inOwner = true;
+  }
+  return inOwner;
+}
+
+function authStatusUI(request, response) {
+  var authStatusUI = '<a href="/login">login</a>'
+  if (authIsOwner(request, response)) {
+    authStatusUI = '<a href="/logout_process">logout</a>'
+  }
+  return authStatusUI;
+}
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
@@ -18,7 +40,8 @@ var app = http.createServer(function(request,response){
           var list = template.list(filelist);
           var html = template.HTML(title, list,
             `<h2>${title}</h2>${description}`,
-            `<a href="/create">create</a>`
+            `<a href="/create">create</a>`,
+            authStatusUI(request, response)
           );
           response.writeHead(200);
           response.end(html);
@@ -40,7 +63,7 @@ var app = http.createServer(function(request,response){
                 <form action="delete_process" method="post">
                   <input type="hidden" name="id" value="${sanitizedTitle}">
                   <input type="submit" value="delete">
-                </form>`
+                </form>`, authStatusUI(request, response)
             );
             response.writeHead(200);
             response.end(html);
@@ -61,7 +84,7 @@ var app = http.createServer(function(request,response){
               <input type="submit">
             </p>
           </form>
-        `, '');
+        `, '', authStatusUI(request, response));
         response.writeHead(200);
         response.end(html);
       });
@@ -98,7 +121,8 @@ var app = http.createServer(function(request,response){
               </p>
             </form>
             `,
-            `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+            `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`,
+            authStatusUI(request, response)
           );
           response.writeHead(200);
           response.end(html);
