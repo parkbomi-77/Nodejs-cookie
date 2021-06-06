97929779943a5e21c147e8df887b04aa855c35ed
parkbomi-77 <vnstn785@naver.com>
2021-06-06 17:10:06 +0900

    express-session 파일스토어 설치

--- nodejs/express-session.js
+++ nodejs/express-session.js
@@ -10,28 +10,17 @@ app.use(session({
 	saveUninitialized: true
 }))
 
-app.use(function (req, res, next) {
-	if (!req.session.views) {
-		req.session.views = {}
+app.get('/', function (req, res, next) {
+	console.log(req.session);
+	if (req.session.num === undefined) {
+		req.session.num = 1;
+	} else {
+		req.session.num = req.session.num + 1;
 	}
-
-	// get the url pathname
-	var pathname = parseurl(req).pathname
-
-	// count the views
-	req.session.views[pathname] = (req.session.views[pathname] || 0) + 1
-
-	next()
-})
-
-app.get('/foo', function (req, res, next) {
-	res.send('you viewed this page ' + req.session.views['/foo'] + ' times')
-})
-
-app.get('/bar', function (req, res, next) {
-	res.send('you viewed this page ' + req.session.views['/bar'] + ' times')
+	res.send(`Views : ${req.session.num}`);
 })
 
 app.listen(3000, function () {
 	console.log('3000!')
 });
+
